bf6e9d426ff27554158708439a7d77a2c9d3278a
Scratchytube <bpsahota@gmail.com>
2020-09-20 14:12:51 -0400

    done

---
+++ lib/post.rb
@@ -0,0 +1,91 @@
+
+class Post 
+
+    attr_accessor :title, :author 
+
+    @@all = [ ]
+
+    def initialize(title)
+        @title = title
+        @@all << self 
+    end
+
+    def self.all 
+        @@all
+    end 
+
+    def author_name 
+        author.name if author 
+    end 
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Post
+#     attr_accessor :title, :author
+  
+#     @@all = []
+  
+#     def initialize(title)
+#       @title = title
+#       @@all << self
+#     end
+  
+#     def self.all
+#       @@all
+#     end
+  
+#     def author_name
+#       author.name if author
+#     end
+#   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
